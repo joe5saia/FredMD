f80b4405446dc15b5e7a64345009d16eef5fb00a
Joe Saia <joe5saia@gmail.com>
2021-01-16 19:54:15 +0000

    add website to setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Estimate factors off of the FRED-MD dataset",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/joe5saia/FredMD",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
