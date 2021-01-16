de747a86575e6fa34f6b0cdd47af8c5bc509c759
Joe Saia <joe5saia@gmail.com>
2021-01-16 20:25:17 +0000

    add tests

---
+++ tests/tests.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as import pd
+import FredMD as fmd
+import pytest
+
+# Model should estimate the maximum number of factors as IC chooses 7 normally
+x = fmd.FredMD(Nfactor=None, vintage=None, maxfactor=4, standard_method=2, ic_method=2)
+assert hasattr(x, 'rawseries')
+x.estimate_factors()
+assert x.factors.shape[1] == 4
+
+# Model should estimate 2 factors since that is what we are setting
+x = fmd.FredMD(Nfactor=2, vintage=None, maxfactor=1, standard_method=2, ic_method=2)
+x.estimate_factors()
+assert x.factors.shape[1] == 2
