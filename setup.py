796e3228a904298bcee5027f3c8cf721d5210c0f
Joseph Saia <joe5saia@gmail.com>
2021-04-21 19:53:54 -0400

    Update setup.py
    Version Bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="FredMD",
-    version="0.0.5",
+    version="0.0.6",
     author="Joe Saia",
     author_email="joe5saia@gmail.com",
     description="Estimate factors off of the FRED-MD dataset",
@@ -18,6 +18,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['pandas>=1.2.0', 'numpy>=1.19.0', 'scikit-learn>=0.24.0'],
+    install_requires=['pandas', 'numpy', 'scikit-learn'],
     python_requires='>=3.6',
 )
